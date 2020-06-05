abf600ac3102a9ca41f6bd9b7fb33b76ed51e137
samzoll22 <sam.zoll22@gmail.com>
2020-06-04 23:04:40 -0500

    starter template

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import List from './components/List.jsx';
+import { Container, Navbar, Jumbotron, Button } from 'react-bootstrap';
+// import List from './components/List.jsx';
 
 class App extends React.Component {
   constructor(props) {
@@ -15,10 +16,27 @@ class App extends React.Component {
   }
 
   render () {
-    return (<div>
-      <h1>Item List</h1>
-      {/* <List items={this.state.items}/> */}
-    </div>)
+    return (
+      <div>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand>
+            the movement
+          </Navbar.Brand>
+        </Navbar>
+        <Container>
+          <Jumbotron style={{marginTop: "20px"}} >
+            <h1>Hello, world!</h1>
+            <p>
+              This is a simple hero unit, a simple jumbotron-style component for calling
+              extra attention to featured content or information.
+            </p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </Jumbotron>
+        </Container>
+      </div>
+    )
   }
 }
 
