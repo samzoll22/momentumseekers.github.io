55555d9a7b35772c588a550dbbe22be5f200fde8
samzoll22 <sam.zoll22@gmail.com>
2020-06-13 17:05:26 -0500

    add favicon and ios message image

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -20,6 +20,13 @@ class App extends React.Component {
       <div>
         <Navbar bg="dark" variant="dark">
           <Navbar.Brand style={{fontFamily: 'Permanent Marker, cursive', fontSize: "2em"}}>
+            <img
+              alt="momentum seekers logo"
+              src="https://zollstorage.s3-us-west-1.amazonaws.com/momentum+seekers/momentum+seekers+logo+%40+200px.png"
+              width="30"
+              height="30"
+              // className="d-inline-block align-top"
+            />{' '}
             momentum seekers
           </Navbar.Brand>
         </Navbar>
