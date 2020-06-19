8f8835cd10db00d3778a538b3e2ef858f59d81fa
samzoll22 <sam.zoll22@gmail.com>
2020-06-18 23:24:38 -0500

    add donate data

---
+++ react-client/src/assets/donate.js
@@ -0,0 +1,112 @@
+const donateData = [
+  {
+    "Name": "Black Lives Matter",
+    "Description": `Black Lives Matter was founded in 2013 in response to the acquittal of Trayvon Martin’s murderer. BLM is a global organization whose mission is to eradicate white supremacy and build local power to intervene in violence inflicted on Black communities by the state and vigilantes.`,
+    "DonateURL": "https://secure.actblue.com/donate/ms_blm_homepage_2019",
+    "HomeURL": "https://blacklivesmatter.com/"
+  },
+  {
+    "Name": "NAACP",
+    "Description": "The vision of the National Association for the Advancement of Colored People is to ensure a society in which all individuals have equal rights without discrimination based on race.",
+    "DonateURL": "https://secure.actblue.com/donate/naacp-1",
+    "HomeURL": "https://www.naacp.org/"
+  },
+  {
+    "Name": "Black Futures Lab",
+    "Description": `Black Futures Lab’s mission is to engage Black voters year-round, use their political strength to stop corporate influences from creeping into progressive policies, and plan to combine technology and traditional organizing methods to reach Black people anywhere and everywhere.`,
+    "DonateURL": "https://cpasf.ourpowerbase.net/civicrm/contribute/transact?reset=1&id=45",
+    "HomeURL": "https://blackfutureslab.org/"
+  },
+  {
+    "Name": "The Okra Project",
+    "Description": "Help The Okra Project support Black Trans people experiencing food insecurity by bringing healthy, home cooked meals to Black Trans people wherever.",
+    "DonateURL": "https://www.artsbusinesscollaborative.org/asp-products/the-okra-project-sponsored-project/",
+    "HomeURL": "https://www.theokraproject.com/"
+  },
+  {
+    "Name": "Protect Black Votes",
+    "Description": "The coronavirus pandemic has devastated voter registration numbers nationwide, and we know that Black voters are some of the most at risk.",
+    "DonateURL": "https://secure.actblue.com/donate/blackvoterregistration?akid=42979.9230500.GUBXy1&rd=1&refcode=dm_fullbase_link1_nondonors&refcode2=42979_9230500_GUBXy1&t=1",
+    "HomeURL": "https://votingwhileblack.com/"
+  },
+  {
+    "Name": "End Police Violence",
+    "Description": "Power the movement to end police violence against black people!",
+    "DonateURL": "https://secure.actblue.com/contribute/page/support-us/?akid=42983.9230500.GMgxRM&amount=20&rd=1&refcode=c3c4emailfooter&refcode2=42983_9230500_GMgxRM&t=2",
+    "HomeURL": "https://colorofchange.org/"
+  },
+  {
+    "Name": "The Equal Justice Initiative",
+    "Description": "Donate to to end mass incarceration and excessive punishment in the United States, challenge racial and economic injustice, and protect the basic human rights!",
+    "DonateURL": "https://support.eji.org/give/153413/#!/donation/checkout",
+    "HomeURL": "https://eji.org/"
+  },
+  {
+    "Name": "The National Bail Fund",
+    "Description": "The National Bail Fund Network is made up of over sixty community bail and bond funds across the country. Donate to your local or national fund.",
+    "DonateURL": "https://www.communityjusticeexchange.org/nbfn-directory",
+    "HomeURL": "https://www.communityjusticeexchange.org/"
+  },
+  {
+    "Name": "Urban Affairs Coalition",
+    "Description": "The UAC unites government, business, neighborhoods, and individual initiatives to improve the quality of life in the region, build wealth in urban communities, and solve emerging issues.",
+    "DonateURL": "https://uac.kindful.com/",
+    "HomeURL": "http://uac.org/"
+  },
+  {
+    "Name": "Chester Education Foundation",
+    "Description": "CEF promotes community revitalization through strategic partnerships in education, youth and work force development, and college access.",
+    "DonateURL": "https://www.chestereducation.org/#",
+    "HomeURL": "https://www.chestereducation.org/#"
+  },
+  {
+    "Name": "Fathers Incorporated",
+    "Description": "FI is a non-profit organization working to build stronger families and communities through the promotion of Responsible Fatherhood.",
+    "DonateURL": "https://www.paypal.me/FathersIncorporated",
+    "HomeURL": "https://www.fathersincorporated.com/"
+  },
+  {
+    "Name": "BYP100",
+    "Description": "BYP100's latest movement campaign is to put an end to the different forms of gender violence that Black women, girls, femmes and gender non-conforming people face everyday.",
+    "DonateURL": "https://secure.actblue.com/donate/byp100-1",
+    "HomeURL": "https://www.byp100.org/"
+  },
+  {
+    "Name": "Save Money Save Life",
+    "Description": "The overall purpose of SMSL is to highlight and empower BIPOC (Black, Indigenous and People of Color). Period.",
+    "DonateURL": "https://www.flipcause.com/secure/cause_pdetails/ODY2ODk=",
+    "HomeURL": "https://www.savemoneysavelife.org/"
+  },
+  {
+    "Name": "ACLU",
+    "Description": "Black people are being murdered and brutalized by police with near impunity. Act with us to end police brutality, demand racial justice, and defend our right to protest.",
+    "DonateURL": "https://action.aclu.org/give/now?ms_aff=NAT&initms_aff=NAT&ms=web_horiz_nav_hp&initms=web_horiz_nav_hp&ms_chan=web&initms_chan=web",
+    "HomeURL": "https://www.aclu.org/"
+  },
+  {
+    "Name": "Growing Home",
+    "Description": "We have a vision of a world of healthy people and communities. Everyone deserves to have a good job, and everyone deserves to eat well.",
+    "DonateURL": "http://growinghomeinc.org/donate/",
+    "HomeURL": "http://growinghomeinc.org/"
+  },
+  {
+    "Name": "Equity First",
+    "Description": "The Equity First mission is centered in transformative justice and healing communities impacted by the War on Drugs.",
+    "DonateURL": "https://www.thesocialimpactcenter.org/equity-first",
+    "HomeURL": "https://www.equityfirstalliance.org/"
+  },
+  {
+    "Name": `Assata’s Daughters (Chicago)`,
+    "Description": "AD organizes young Black people in Chicago by providing them with political education, leadership development, mentorship, and revolutionary services.",
+    "DonateURL": "https://donorbox.org/nov-2016-push",
+    "HomeURL": "https://www.assatasdaughters.org/"
+  },
+  {
+    "Name": "Chicago Bond Fund",
+    "Description": "CCBF pays bond for people charged with crimes in Cook County, Illinois.",
+    "DonateURL": "https://chicagobond.org/donate/",
+    "HomeURL": "https://chicagobond.org/"
+  }
+]
+
+export default donateData
--- react-client/src/components/Donate.jsx
+++ react-client/src/components/Donate.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import donateData from '../assets/donate.js'
 import { Container, Row, Col, Button } from 'react-bootstrap';
 
 const Donate = (props) => (
@@ -9,62 +10,26 @@ const Donate = (props) => (
                 <h2 style={{fontFamily: 'Permanent Marker, cursive', textAlign: "center", color: "#fff"}}>Donate 💸</h2>
             </Col>
         </Row>
-        <Row style={{marginTop: "30px"}}>
-            <Col>
-                <h5 style={{fontFamily: 'Coustard, serif', color: "#fff"}}>Black Lives Matter</h5>
-                <p style={{fontFamily: 'Coustard, serif', color: "#fff"}}>Black Lives Matter was founded in 2013 in response to the acquittal of Trayvon Martin’s murderer. BLM is a global organization whose mission is to eradicate white supremacy and build local power to intervene in violence inflicted on Black communities by the state and vigilantes.</p>
-                <Button href="https://secure.actblue.com/donate/ms_blm_homepage_2019" block size="lg" style={{height: "5vh", fontFamily: 'Coustard, serif'} } variant="outline-warning" >Donate Now</Button>
-            </Col>
-        </Row>
-        <Row style={{marginTop: "30px"}}>
-            <Col>
-                <h5 style={{fontFamily: 'Coustard, serif', color: "#fff"}}>NAACP</h5>
-                <p style={{fontFamily: 'Coustard, serif', color: "#fff"}}>The vision of the National Association for the Advancement of Colored People is to ensure a society in which all individuals have equal rights without discrimination based on race.</p>
-                <Button href="https://secure.actblue.com/donate/naacp-1" block size="lg" style={{height: "5vh", fontFamily: 'Coustard, serif'} } variant="outline-warning" >Donate Now</Button>
-            </Col>
-        </Row>
-        <Row style={{marginTop: "30px"}}>
-            <Col>
-                <h5 style={{fontFamily: 'Coustard, serif', color: "#fff"}}>Black Futures Lab</h5>
-                <p style={{fontFamily: 'Coustard, serif', color: "#fff"}}>Black Futures Lab’s mission is to engage Black voters year-round, use their political strength to stop corporate influences from creeping into progressive policies, and plan to combine technology and traditional organizing methods to reach Black people anywhere and everywhere.</p>
-                <Button href="https://cpasf.ourpowerbase.net/civicrm/contribute/transact?reset=1&id=45" block size="lg" style={{height: "5vh", fontFamily: 'Coustard, serif'} } variant="outline-warning" >Donate Now</Button>
-            </Col>
-        </Row>
-        <Row style={{marginTop: "30px"}}>
-            <Col>
-                <h5 style={{fontFamily: 'Coustard, serif', color: "#fff"}}>The Okra Project</h5>
-                <p style={{fontFamily: 'Coustard, serif', color: "#fff"}}>Help The Okra Project support Black Trans people experiencing food insecurity by bringing healthy, home cooked meals to Black Trans people wherever.</p>
-                <Button href="https://www.theokraproject.com/" block size="lg" style={{height: "5vh", fontFamily: 'Coustard, serif'} } variant="outline-warning" >Donate Now</Button>
-            </Col>
-        </Row>
-        <Row style={{marginTop: "30px"}}>
-            <Col>
-                <h5 style={{fontFamily: 'Coustard, serif', color: "#fff"}}>Protect Black Votes</h5>
-                <p style={{fontFamily: 'Coustard, serif', color: "#fff"}}>The coronavirus pandemic has devastated voter registration numbers nationwide, and we know that Black voters are some of the most at risk.</p>
-                <Button href="https://secure.actblue.com/donate/blackvoterregistration?akid=42979.9230500.GUBXy1&rd=1&refcode=dm_fullbase_link1_nondonors&refcode2=42979_9230500_GUBXy1&t=1" block size="lg" style={{height: "5vh", fontFamily: 'Coustard, serif'} } variant="outline-warning" >Donate Now</Button>
-            </Col>
-        </Row>
-        <Row style={{marginTop: "30px"}}>
-            <Col>
-                <h5 style={{fontFamily: 'Coustard, serif', color: "#fff"}}>End Police Violence</h5>
-                <p style={{fontFamily: 'Coustard, serif', color: "#fff"}}>Power the movement to end police violence against black people!</p>
-                <Button href="https://secure.actblue.com/contribute/page/support-us/?akid=42983.9230500.GMgxRM&amount=20&rd=1&refcode=c3c4emailfooter&refcode2=42983_9230500_GMgxRM&t=2" block size="lg" style={{height: "5vh", fontFamily: 'Coustard, serif'}} variant="outline-warning">Donate Now</Button>
-            </Col>
-        </Row>
-        <Row style={{marginTop: "30px"}}>
-            <Col>
-                <h5 style={{fontFamily: 'Coustard, serif', color: "#fff"}}>The Equal Justice Initiative</h5>
-                <p style={{fontFamily: 'Coustard, serif', color: "#fff"}}>Donate to to end mass incarceration and excessive punishment in the United States, challenge racial and economic injustice, and protect the basic human rights!</p>
-                <Button href="https://support.eji.org/give/153413/#!/donation/checkout" block size="lg" style={{height: "5vh", fontFamily: 'Coustard, serif'}} variant="outline-warning">Donate Now</Button>
-            </Col>
-        </Row>
-        <Row style={{marginTop: "30px"}}>
-            <Col>
-                <h5 style={{fontFamily: 'Coustard, serif', color: "#fff"}}>The National Bail Fund</h5>
-                <p style={{fontFamily: 'Coustard, serif', color: "#fff"}}>The National Bail Fund Network is made up of over sixty community bail and bond funds across the country. Donate to your local or national fund.</p>
-                <Button href="https://www.communityjusticeexchange.org/nbfn-directory" block size="lg" style={{height: "5vh", fontFamily: 'Coustard, serif'}} variant="outline-warning">Donate Now</Button>
-            </Col>
-        </Row>
+        { 
+                            donateData.map((donation, i) => {
+                                return(
+                                <Row key={i} style={{marginTop: "30px"}}>
+                                    <Col>
+                                        <h5 style={{fontFamily: 'Coustard, serif', color: "#fff"}}>{donation.Name}</h5>
+                                        <p style={{fontFamily: 'Coustard, serif', color: "#fff"}}>{donation.Description}</p>
+                                        <Row>
+                                            <Col>
+                                                <Button href={donation.HomeURL} block size="lg" style={{height: "5vh", fontFamily: 'Coustard, serif'} } variant="warning" >Check It Out</Button>
+                                            </Col>
+                                            <Col>
+                                                <Button href={donation.DonateURL} block size="lg" style={{height: "5vh", fontFamily: 'Coustard, serif'} } variant="warning" >Donate Now</Button>
+                                            </Col>
+                                        </Row>
+                                    </Col>
+                                </Row>
+                                )
+                            })
+                        }
         <Row style={{marginTop: "30px", marginBottom: "40px"}}>
             <Col>
                 <Button value="homePage" onClick={props.handleStateChange} block style={{height: "5vh", fontFamily: 'Coustard, serif'}} variant="outline-light" >Home 🏡</Button>
